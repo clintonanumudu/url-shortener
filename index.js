d670f0e5191528a992bacfaaac3b37e2e7b44e4b
clintonanumudu <54355338+clintonanumudu@users.noreply.github.com>
2021-01-08 06:05:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+var fs = require("fs");
+
+var bodyParser = require("body-parser");
+
+var validUrl = require('valid-url');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(function (req, res) {
+	
+	if (req.body.url) {
+		
+		var urlinput = req.body.url;
+		
+		var urlid = "";
+		
+		function generateId() {
+			
+			for (i = 0; i < 6; i++) {
+				
+				var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				
+				var random = Math.floor(Math.random() * characters.length);
+				
+				urlid = urlid + characters.charAt(random);
+				
+			}
+			
+			fs.readFile("links.txt", "utf8", (err, links) => {
+				
+				if (links.indexOf(urlid) == -1) {
+					
+					fs.appendFile("links.txt", urlid + " | " + urlinput + "\n", function(){});
+					
+					res.send(req.headers.host + "/" + urlid);
+					
+				}
+				
+				else {
+					
+					console.log("Id already exists")
+					
+					generateId();
+					
+				}
+				
+			});
+			
+		}
+		
+		if (validUrl.isUri(urlinput)){
+			
+			generateId();
+			
+		}
+		
+		else {
+			
+			res.send("Invalid URL");
+			
+		}
+		
+	}
+	
+	else {
+		
+		if (req.url !== "/") {
+			
+			var urlid = req.url.substring(1);
+			
+			fs.readFile("links.txt", "utf8", (err, links) => {
+				
+				if (links.indexOf(urlid) !== -1) {
+					
+					var redirect = links.substring(links.indexOf(urlid) + urlid.length + " | ".length, links.indexOf("\n", links.indexOf(urlid) + urlid.length + " | ".length));
+					
+					res.send("<script type='text/javascript'> location.href = '" + redirect + "' </script>");
+					
+				}
+				
+				else {
+					
+					res.send("Not Found");
+					
+				}
+				
+			});
+			
+		}
+		
+		else {
+			
+			res.sendFile("index.html", {root: __dirname});
+			
+		}
+	
+	}
+	
+});
+
+app.listen(port, () => {
+	
+	console.log("Server listening on port: " + port);
+	
+});
